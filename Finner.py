672e5c2909980fe45dedc2104782404bf971833a
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-10-26 00:38:34 -0400

    Update Finner.py

--- Finner.py
+++ Finner.py
@@ -333,7 +333,7 @@ def main():
                 st.write(f"**10-K URL:** [Link]({data['10-K_URL']})")
             
                 # Displaying Logo (resized)
-                st.image(data['Logo_URL'], caption=f"Logo for {data['Symbol']}", width=100, output_format='PNG', style={'border-radius': '50%'})
+                st.markdown(f'<img src="{data['Logo_URL']}" alt="Logo for {data['Symbol']}" style="border-radius:50%;" width=100>', unsafe_allow_html=True)
                 
                 st.write("**Chart:**")
                 candlestick_data = fetch_candlestick_data(ticker)
