e2f5179e70657ef0c8aa0d401c34fda9c2e322f8
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-17 19:16:37 -0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -27,48 +27,48 @@ def analysis_page():
         embeddings = OpenAIEmbeddings()
 
         # Upload file
-uploaded_file = st.file_uploader("Upload your annual report PDF", type="pdf")
-    if uploaded_file:
-        # Save the uploaded file to a temporary file
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-            tmp_file.write(uploaded_file.read())
-            temp_file_path = tmp_file.name
-
-        # Now use this temporary file path with PyPDFLoader
-        loader = PyPDFLoader(temp_file_path)
-            # Load documents into ChromaDB
-            store = Chroma.from_documents(pages, embeddings, collection_name='annualreport')
-
-            # Create vectorstore info object
-            vectorstore_info = VectorStoreInfo(
-                name="annual_report",
-                description="A document uploaded by the user",
-                vectorstore=store
-            )
-
-            # Convert the document store into a langchain toolkit
-            toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-
-            # Add the toolkit to an end-to-end LC
-            agent_executor = create_vectorstore_agent(
-                llm=llm,
-                toolkit=toolkit,
-                verbose=True
-            )
-
-            # Create a text input box for the user
-            prompt = st.text_input('Input your prompt here')
-
-            # If the user inputs a prompt
-            if prompt:
-                # Then pass the prompt to the LLM
-                response = agent_executor.run(prompt)
-                # ...and write it out to the screen
-                st.write(response)
-
-                # With a streamlit expander  
-                with st.expander('Document Similarity Search'):
-                    # Find the relevant pages
-                    search = store.similarity_search_with_score(prompt) 
-                    # Write out the first 
-                    st.write(search[0][0].page_content)
+        uploaded_file = st.file_uploader("Upload your annual report PDF", type="pdf")
+            if uploaded_file:
+                # Save the uploaded file to a temporary file
+                with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+                    tmp_file.write(uploaded_file.read())
+                    temp_file_path = tmp_file.name
+        
+                # Now use this temporary file path with PyPDFLoader
+                loader = PyPDFLoader(temp_file_path)
+                # Load documents into ChromaDB
+                store = Chroma.from_documents(pages, embeddings, collection_name='annualreport')
+    
+                # Create vectorstore info object
+                vectorstore_info = VectorStoreInfo(
+                    name="annual_report",
+                    description="A document uploaded by the user",
+                    vectorstore=store
+                )
+    
+                # Convert the document store into a langchain toolkit
+                toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+    
+                # Add the toolkit to an end-to-end LC
+                agent_executor = create_vectorstore_agent(
+                    llm=llm,
+                    toolkit=toolkit,
+                    verbose=True
+                )
+    
+                # Create a text input box for the user
+                prompt = st.text_input('Input your prompt here')
+    
+                # If the user inputs a prompt
+                if prompt:
+                    # Then pass the prompt to the LLM
+                    response = agent_executor.run(prompt)
+                    # ...and write it out to the screen
+                    st.write(response)
+    
+                    # With a streamlit expander  
+                    with st.expander('Document Similarity Search'):
+                        # Find the relevant pages
+                        search = store.similarity_search_with_score(prompt) 
+                        # Write out the first 
+                        st.write(search[0][0].page_content)
