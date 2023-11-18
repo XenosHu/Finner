5a8bb37de0164e9e4bf4d20abfb054e6ed6971e5
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-17 19:02:18 -0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,72 +1,77 @@
-
 def analysis_page():
     import os
     import tempfile
     from langchain.llms import OpenAI
     from langchain.embeddings import OpenAIEmbeddings
     import streamlit as st
-    from langchain.document_loaders import PyPDFLoader
+    from langchain.document_loaders import PyPDFLoader, HTMLLoader  # Assuming HTMLLoader is available
     from langchain.vectorstores import Chroma
     from langchain.agents.agent_toolkits import (
         create_vectorstore_agent,
         VectorStoreToolkit,
         VectorStoreInfo
     )
+
     st.title('GPT Financial Report Analyzer')
-    
+
     # User inputs their OpenAI API key
     api_key = st.text_input("Enter your OpenAI API key", type="password")
-    
+
     if api_key:
         # Set the API key environment variable
         os.environ['OPENAI_API_KEY'] = api_key
-    
+
         # Create instance of OpenAI LLM
         llm = OpenAI(temperature=0.1, verbose=True)
         embeddings = OpenAIEmbeddings()
-    
-        # Upload PDF file
-        uploaded_file = st.file_uploader("Upload your annual report PDF", type="pdf")
+
+        # Upload file
+        uploaded_file = st.file_uploader("Upload your document (PDF or HTML)", type=["pdf", "html", "htm"])
         if uploaded_file:
+            # Determine file type and process accordingly
+            file_type = uploaded_file.type
+            suffix = ".pdf" if file_type == "application/pdf" else ".html"
+            loader_class = PyPDFLoader if file_type == "application/pdf" else HTMLLoader
+
             # Save the uploaded file to a temporary file
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp_file:
                 tmp_file.write(uploaded_file.read())
                 temp_file_path = tmp_file.name
-    
-            # Now use this temporary file path with PyPDFLoader
-            loader = PyPDFLoader(temp_file_path)
-            # Split pages from pdf 
+
+            # Load document using appropriate loader
+            loader = loader_class(temp_file_path)
             pages = loader.load_and_split()
-            # Load documents into vector database aka ChromaDB
+
+            # Load documents into ChromaDB
             store = Chroma.from_documents(pages, embeddings, collection_name='annualreport')
-    
+
             # Create vectorstore info object
             vectorstore_info = VectorStoreInfo(
                 name="annual_report",
-                description="a banking annual report as a pdf",
+                description="A document uploaded by the user",
                 vectorstore=store
             )
-    
+
             # Convert the document store into a langchain toolkit
             toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-    
+
             # Add the toolkit to an end-to-end LC
             agent_executor = create_vectorstore_agent(
                 llm=llm,
                 toolkit=toolkit,
                 verbose=True
             )
-    
+
             # Create a text input box for the user
             prompt = st.text_input('Input your prompt here')
-    
+
             # If the user inputs a prompt
             if prompt:
                 # Then pass the prompt to the LLM
                 response = agent_executor.run(prompt)
                 # ...and write it out to the screen
                 st.write(response)
-    
+
                 # With a streamlit expander  
                 with st.expander('Document Similarity Search'):
                     # Find the relevant pages
